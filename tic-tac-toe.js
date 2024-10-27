a276262faa86e5a5cf7a60390d9b53822d4c626a
Reneece Bartley <Ren@Glendons-MacBook.local>
2024-10-27 14:59:43 -0500

    Implement layout for the Tic Tac Toe board

---
+++ tic-tac-toe.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const squares = document.querySelectorAll('#board div');
+    squares.forEach(square => {
+        square.classList.add('square');
+    });
+});
